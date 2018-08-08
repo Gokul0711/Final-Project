f2bbb119b66b11007a6590453df6c76b74d66578
JDavidMoreno <david.moreno0440@gmail.com>
2018-08-08 14:40:11 +0200

    Models Profiles and Trees created

--- gotrees/models.py
+++ gotrees/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profiles(models.Model):
+    image = models.CharField(max_length=30,blank=True)
+    country = models.CharField(max_length=30, blank=True)
+    region = models.CharField(max_length=30, blank=True)
+    my_phrase = models.CharField(max_length=100, blank=True)
+    my_text = models.CharField(max_length=400, blank=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="profile")
+
+    def __str__(self):
+        return f"{self.country} - {self.region} - {self.my_phrase} - {self.my_text}"
+
+class Trees(models.Model):
+    type = models.CharField(max_length=30)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="trees")
+    time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.type} - {self.user_id} - {self.time}"
--- gotrees/views.py
+++ gotrees/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from .models import Profiles, Trees
 
 # Create your views here.
 def index(request):
@@ -51,7 +52,17 @@ def logout_page(request):
 
 def myforest(request, user_username):
 
-    return render(request, "gotrees/myforest.html")
+    user = User.objects.get(username=user_username)
+
+    context = {
+    "user_username": user_username,
+    "first_name": user.first_name,
+    "country": user.profile.all()[0].country,
+    "region": user.profile.all()[0].region,
+    "my_phrase": user.profile.all()[0].my_phrase,
+    "my_text": user.profile.all()[0].my_text
+    }
+    return render(request, "gotrees/myforest.html", context)
 
 
 def edit_profile(request, user_username):
@@ -60,10 +71,22 @@ def edit_profile(request, user_username):
     if request.user.username == user_username:
         if request.method == 'POST':
             name = request.POST["name"]
+            my_phrase = request.POST["my_phrase"]
             country = request.POST["country"]
             region = request.POST["region"]
+            my_text = request.POST["my_text"]
 
-            return HttpResponseRedirect(reverse('myforest'))
+            user = User.objects.get(id=request.user.id)
+            user.first_name = name
+            user.save()
+
+            if len(user.profile.all()) == 0:
+                profile = Profiles.objects.create(country=country, region=region, my_phrase=my_phrase, my_text=my_text, user_id=user)
+                profile.save()
+                return HttpResponseRedirect(reverse('myforest', kwargs={"user_username": user_username}))
+            else:
+                Profiles.objects.filter(user_id=user).update(country=country, region=region, my_phrase=my_phrase, my_text=my_text)
+                return HttpResponseRedirect(reverse('myforest', kwargs={"user_username": user_username}))
         else:
             return render(request, 'gotrees/edit_profile.html')
 
